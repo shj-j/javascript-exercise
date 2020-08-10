bfc6d82e2c4f4354624fa55ba57f9c7d47b76973
shj-j <shang.jie@outlook.com>
2020-08-10 18:51:14 +0800

    [sj]:done class

--- 4-class/practice_4/student.js
+++ 4-class/practice_4/student.js
@@ -1,1 +1,14 @@
 // TODO 16: 在这里写实现代码
+// TODO 13: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,9 @@
 // TODO 17: 在这里写实现代码
+// TODO 14: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  introduce() {
+    return `My name is ${this.name}. I am ${this.age} years old. I am a Worker. I have a job.`;
+  }
+}
