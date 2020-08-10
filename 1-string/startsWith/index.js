c693655943796dffa6a6a9ab187bee888b9c68be
shj-j <shang.jie@outlook.com>
2020-08-10 15:32:26 +0800

    [sj]；Done String

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,5 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  const resultList = collection.filter(c => c.includes('粤A'));
+  return resultList.length;
 }
--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,6 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const result = `你好，刘明！你的考试成绩为${getScore(person.id)}分`;
+  return result;
 }
