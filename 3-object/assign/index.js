874cb94c45b5382510a8910a19bb1c375a35fd27
shj-j <shang.jie@outlook.com>
2020-08-10 16:34:49 +0800

    [sj]: done object

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,13 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const newObj = {
+    serialNumber: '12345',
+    type: 'accessory',
+    properties: {
+      color: 'green',
+      status: 'processed'
+    }
+  };
+  const result = Object.assign(source, newObj);
+  return result;
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,5 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const result = Number(source.A) + Number(source.B) + Number(source.C);
+  return result;
 }
